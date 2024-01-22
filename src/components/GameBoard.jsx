27d0cb6ce8db98660decec539511ec61df021394
이효섭 <hyosube@hancom.com>
2024-01-22 11:30:00 +0900

    [수정] Log Component 추가 gameturn 상태 리팩토리

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 const initialGameBoard = [
     [null, null, null],
@@ -6,17 +5,14 @@ const initialGameBoard = [
     [null, null, null],
 ]
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol} ) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({onSelectSquare, turns} ) {
+    let gameBoard = initialGameBoard;
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard)=> {
-            const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updateBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updateBoard;
-        });
+    for (const turn of turns) {
+        const {square, player} = turn;
+        const {row, col} = square;
 
-        onSelectSquare();
+        gameBoard[row][col] = player;
     }
 
     return (
@@ -26,7 +22,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol} ) {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
